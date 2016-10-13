418a4c78524a3b69a5a52c3f8100da018d29e780
Sofia Lunkes <sofia.big@live.com>
2016-10-13 13:31:58 -0300

    revisao

--- src/main/java/br/com/enriquecimento/Main.java
+++ src/main/java/br/com/enriquecimento/Main.java
@@ -2,8 +2,11 @@ package br.com.enriquecimento;
 
 import br.com.enriquecimento.leitor.*;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -13,21 +16,33 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		FileInputStream fileInput = new FileInputStream("/home/sofia/Downloads/arquivo_retorno.csv");
+		FileInputStream fileInput = new FileInputStream("/home/sofia/Downloads/enriquecimento/arquivo_retorno.csv");
 		InputStreamReader reader = new InputStreamReader(fileInput);
 		BufferedReader br = new BufferedReader(reader);
+		File arquivo = new File("/home/sofia/Downloads/enriquecimento/atualiza_2.sql");
+
+		boolean existe = arquivo.exists();
+		if (!existe) {
+			arquivo.createNewFile();
+		}
+		FileWriter fw = new FileWriter(arquivo);
+		BufferedWriter bw = new BufferedWriter(fw);
+
 		String linha = br.readLine();
+		String dataAlteracao = "11/10/2016";
 		int cont = 0;
 
+		Associado associado = new Associado();
+		String tabelaAssociado="TBOD_ASSOCIADO_LXTMP";
+		String tabelaHistoricoAssociado = "TBOD_HISTORICO_ASSOCIADO_LXTMP";
 		while (linha != null) {
-			Associado associado = new Associado();
 			StringBuilder sqlInsert = new StringBuilder();
 			StringBuilder sqlUpdate = new StringBuilder();
 			StringBuilder sqlHistorico = new StringBuilder();
 			boolean alterou = false;
 
-			sqlInsert.append("INSERT INTO TBOD_HISTORICO_ASSOCIADO(CD_ASSOCIADO, DT_ALTERACAO,");
-			sqlUpdate.append("UPDATE TBOD_ASSOCIADO SET ");
+			sqlInsert.append("INSERT INTO "+tabelaHistoricoAssociado+"(CD_ASSOCIADO, DT_ALTERACAO,");
+			sqlUpdate.append("UPDATE "+tabelaAssociado+" SET ");
 
 			String[] dados = linha.split(";");
 
@@ -55,10 +70,10 @@ public class Main {
 					|| associado.getFlagNomeCf().equals("NOME ATRIBUIDO PELA CREDIFY")) {
 				sqlUpdate.append(" ID_NOME_SERASA = 'S', NM_ASSOCIADO = '" + associado.getNomeCf() + "'");
 				sqlInsert.append(" NM_ASSOCIADO");
-				System.out.println(sqlInsert.toString());
 				sqlHistorico.append("'" + associado.getNomeCf() + "'");
 				alterou = true;
 			}
+
 			// verificar data de nascimento
 			if (associado.getFlagDtNascimentoCf().equals("DATA DE NASCIMENTO CORRIGIDO PELA CREDIFY")) {
 				if (alterou) {
@@ -66,7 +81,25 @@ public class Main {
 				}
 				sqlUpdate.append(" DT_NASCIMENTO = '" + associado.getDtNascimentoCf() + "' ");
 
-				if (!sqlInsert.toString().equals("INSERT INTO TBOD_HISTORICO_ASSOCIADO(CD_ASSOCIADO, DT_ALTERACAO,")) {
+				if (!sqlInsert.toString().equals("INSERT INTO "+tabelaHistoricoAssociado+"(CD_ASSOCIADO, DT_ALTERACAO,")) {
+					sqlInsert.append(", ");
+				}
+				sqlInsert.append("DT_NASCIMENTO");
+
+				if (!sqlHistorico.equals("")) {
+					sqlHistorico.append(", ");
+				}
+				sqlHistorico.append("'" + associado.getDtNascimentoCf() + "'");
+				alterou = true;
+			}
+			
+			if (associado.getFlagDtNascimentoCf().equals("DATA DE NASCIMENTO ATRIBUIDO PELA CREDIFY")) {
+				if (alterou) {
+					sqlUpdate.append(", ");
+				}
+				sqlUpdate.append(" DT_NASCIMENTO = '" + associado.getDtNascimentoCf() + "' ");
+
+				if (!sqlInsert.toString().equals("INSERT INTO "+tabelaHistoricoAssociado+"(CD_ASSOCIADO, DT_ALTERACAO,")) {
 					sqlInsert.append(", ");
 				}
 				sqlInsert.append("DT_NASCIMENTO");
@@ -86,13 +119,12 @@ public class Main {
 				sqlUpdate.append("ID_CPF_SERASA = 'S' ");
 				alterou = true;
 			}
-
 			if (associado.getFlagCpfCf().equals("CPF CORRIGIDO PELA CREDIFY")) {
 				if (alterou) {
 					sqlUpdate.append(", ");
 				}
 				sqlUpdate.append(" ID_CPF_SERASA = 'S', NR_CPF='" + associado.getCpfCf() + "'");
-				if (!sqlInsert.toString().equals("INSERT INTO TBOD_HISTORICO_ASSOCIADO(CD_ASSOCIADO, DT_ALTERACAO,")) {
+				if (!sqlInsert.toString().equals("INSERT INTO "+tabelaHistoricoAssociado+"(CD_ASSOCIADO, DT_ALTERACAO,")) {
 					sqlInsert.append(", ");
 				}
 				sqlInsert.append("NR_CPF");
@@ -104,12 +136,11 @@ public class Main {
 				alterou = true;
 
 			}
-
 			if (associado.getFlagCpfCf().equals("CPF ATRIBUIDO PELA CREDIFY")) {
 				if (alterou) {
 					sqlUpdate.append(", ");
 				}
-				if (!sqlInsert.toString().equals("INSERT INTO TBOD_HISTORICO_ASSOCIADO(CD_ASSOCIADO, DT_ALTERACAO,")) {
+				if (!sqlInsert.toString().equals("INSERT INTO "+tabelaHistoricoAssociado+"(CD_ASSOCIADO, DT_ALTERACAO,")) {
 					sqlInsert.append(", ");
 				}
 				sqlInsert.append("NR_CPF");
@@ -119,28 +150,85 @@ public class Main {
 				sqlHistorico.append("'" + associado.getCpfCf() + "'");
 				alterou = true;
 			}
-			
-			//verificar o nome da mae
-			if(associado.getFlagNomeMaeCf().equals("NOME MAE ORIGINAL VALIDADO NA BASE CREDIFY")){
-				if(alterou){
+
+			// verificar o nome da mae
+			if (associado.getFlagNomeMaeCf().equals("NOME MAE ORIGINAL VALIDADO NA BASE CREDIFY")) {
+				if (alterou) {
 					sqlUpdate.append(", ");
 				}
 				sqlUpdate.append(" ID_NOME_MAE_SERASA = 'S' ");
 				alterou = true;
 			}
-
-			if(associado.getFlagNomeMaeCf().equals("NOME MAE CORRIGIDO PELA CREDIFY")){
-				if(alterou) {
+			if (associado.getFlagNomeMaeCf().equals("NOME MAE CORRIGIDO PELA CREDIFY")) {
+				if (alterou) {
 					sqlUpdate.append(", ");
 				}
-				sqlUpdate.append("ID_NOME_MAE_SERASA = 'S', NM_MAE_ASSOCIADO = '"+associado.getNomeMaeCf()+"'");
+				sqlUpdate.append("ID_NOME_MAE_SERASA = 'S', NM_MAE_ASSOCIADO = '" + associado.getNomeMaeCf() + "' ");
+
+				if (!sqlInsert.toString().equals("INSERT INTO "+tabelaHistoricoAssociado+"(CD_ASSOCIADO, DT_ALTERACAO,")) {
+					sqlInsert.append(", ");
+				}
+				sqlInsert.append("NM_MAE_ASSOCIADO");
+
+				if (!sqlHistorico.equals("")) {
+					sqlHistorico.append(", ");
+				}
+				sqlHistorico.append("'" + associado.getNomeMaeCf() + "'");
+				alterou = true;
 			}
+			if (associado.getFlagNomeMaeCf().equals("NOME MAE ATRIBUIDO PELA CREDIFY")) {
+				if (alterou) {
+					sqlUpdate.append(", ");
+				}
+				sqlUpdate.append("ID_NOME_MAE_SERASA ='S', NM_MAE_ASSOCIADO= '" + associado.getNomeCf() + "' ");
+				if (!sqlInsert.toString().equals("INSERT INTO "+tabelaHistoricoAssociado+"(CD_ASSOCIADO, DT_ALTERACAO,")) {
+					sqlInsert.append(", ");
+				}
+				sqlInsert.append("NM_MAE_ASSOCIADO");
+
+				if (!sqlHistorico.equals("")) {
+					sqlHistorico.append(", ");
+				}
+				sqlHistorico.append("'" + associado.getNomeMaeCf() + "'");
+				alterou = true;
+			}
+
+			if (alterou) {
+				sqlUpdate.append(", ");
+				sqlUpdate.append("DT_ALTERACAO = '" + dataAlteracao + "' WHERE CD_ASSOCIADO='"
+						+ associado.getCdAssociado() + "';");
+				if (!sqlInsert.toString().equals("INSERT INTO "+tabelaHistoricoAssociado+"(CD_ASSOCIADO, DT_ALTERACAO,")) {
+					sqlInsert.append(") VALUES ('" + associado.getCdAssociado() + "', '" + dataAlteracao + "', "
+							+ sqlHistorico.toString() + ");");
+					bw.write(sqlUpdate.toString());
+					bw.newLine();
+					bw.write(sqlInsert.toString());
+					bw.newLine();
+				}else{
+					bw.write(sqlUpdate.toString());
+					bw.newLine();
+				}
+
+			}
+			
+			
+			cont++;
+			if(cont>=2000){
+				bw.write("commit;");
+				bw.newLine();
+				cont=0;
+			}
+			
+			
 			linha = br.readLine();
 			if (cont == 3) {
 				break;
 			}
 
 		}
-
+		br.close();
+		bw.close();
+		fw.close();
+		reader.close();
 	}
 }
